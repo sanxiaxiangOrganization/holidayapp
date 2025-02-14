743bf989a3eeea8aca38bcbca8284074d7a65461
1234 <1447365568@qq.com>
2025-02-14 21:35:01 +0800

    完善编辑资料+景点详情+评论打分

--- miniprogram-3/pages/landscape/landscape.js
+++ miniprogram-3/pages/landscape/landscape.js
@@ -13,13 +13,13 @@ Page({
     duration: 500,
 
 
-    landscapeList:[
-      {picture:'/images/A (1).jpg',name:'县工委旧址'},
-      {picture:'/images/dhz/4.jpg',name:'大湖寨'},
-      {picture:'/images/hxw/1.jpg',name:'何新屋'},
-      {picture:'/images/hxw/1.jpg',name:'何新屋'},
-      {picture:'/images/hxw/1.jpg',name:'何新屋'}
-    ],
+    landscapeList:[]/*//不要看这个，这不是示例[
+      {pic_url:'/images/A (1).jpg',name:'县工委旧址'},
+      {pic_url:'/images/dhz/4.jpg',name:'大湖寨'},
+      {pic_url:'/images/hxw/1.jpg',name:'何新屋'},
+      {pic_url:'/images/hxw/1.jpg',name:'何新屋'},
+      {pic_url:'/images/hxw/1.jpg',name:'何新屋'}
+    ]*/
   },
   changeIndicatorDots() {
     this.setData({
@@ -27,12 +27,16 @@ Page({
     })
   },
   navigateToDetail(e){
+    var that=this
+    console.log("e:",e.currentTarget);
     /*将对应景点的名字当参数传给页面*/
     const name=e.currentTarget.dataset.name
+    const id=e.currentTarget.dataset.id
+    const picture=e.currentTarget.dataset.picture
     wx.navigateTo({
-      url: `../landscape_detail/landscape_detail?name=${name}`,
+      url: `../landscape_detail/landscape_detail?name=${name}&id=${id}&score=${that.data.landscapeList[e.currentTarget.dataset.index].score}&picture=${picture}`,
     })
-    console.log("=================用户要查看",name,"的详细信息==============");
+    console.log("——————————————————————————————用户要查看",name,"的详细信息————————————————————————————");
   },
   changeAutoplay() {
     this.setData({
@@ -55,6 +59,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
+    var that=this
     var app=getApp()
     //如果用户是第一次登录，就提示用户绑定手机号
     if (app.globalData.firstLogin) {
@@ -64,7 +69,7 @@ Page({
         showCancel: true,
         cancelText: '暂不绑定',
         confirmText: '去绑定',
-        confirmColor: '#007AFF',
+        confirmColor: '#43c74c',
         success(res) {
           if (res.confirm) {
             wx.navigateTo({
@@ -74,8 +79,72 @@ Page({
         }
       });
     }
+    //获取所有景点
+   that.getAllLand().then(() => {
+    // 获取到景点数据后，再获取分数
+    that.getAllScore();
+  }).catch((error) => {
+    console.error("获取景点数据失败:", error);
+  });
   },
 
+  //获取所有景点
+  getAllLand(){
+    var that=this
+    // 创建一个 Promise 包装异步请求
+  return new Promise((resolve, reject) => {
+    //获取所有景点的信息
+    wx.request({
+      url: 'http://localhost:8080/landscape/getAll',//后端获取所有景点的接口
+      method: 'GET',
+       header:{
+        'content-type': 'application/x-www-form-urlencoded',
+      },
+      success(res) {
+        console.log('获取所有景点信息的接口请求成功:', res.data.data);
+        that.setData({
+          landscapeList:res.data.data
+        }) 
+        // 请求成功，执行 resolve
+        resolve();
+     },
+      fail(error) {
+        // 处理登录失败的情况
+        console.log('获取所有景点信息失败:', error);
+        wx.showToast({
+            title: '网络错误，请重启小程序',
+            icon: 'none',
+            duration: 2000
+        });
+        // 请求失败，执行 reject
+        reject(error);
+      }
+    })
+  });
+},
+
+  //获取每个景点的得分
+  getAllScore(){
+    var that=this
+    const app=getApp()
+    console.log("————————————————————————获取每个景点的分数————————————————————");
+    //继而获取每个景点的分数
+    for (let i = 0; i < that.data.landscapeList.length; i++) {  
+     // 为每个景点添加 score 属性并赋值
+     console.log("第",i+1,"个景点的名字是：",that.data.landscapeList[i].name);
+     app.getScore(that.data.landscapeList[i].landscape_id).then(score => {
+      const landscapes = that.data.landscapeList; // 获取当前页面的数组数据
+      landscapes[i].score = score; // 将评分赋值给第 i 个景点对象的 score 字段
+
+      that.setData({
+        landscapeList: landscapes, // 更新数组数据，页面会重新渲染
+      });
+    }).catch(error => {
+      console.log('请求失败:', error);
+    });
+  }
+  },
+  
    /**
    * 生命周期函数--监听页面显示
    */
--- miniprogram-3/pages/landscape_detail/landscape_detail.js
+++ miniprogram-3/pages/landscape_detail/landscape_detail.js
@@ -6,7 +6,10 @@ Page({
    */
   data: {
     //哪个景点
-    myLandscapeName:"大湖寨",
+    myLandscapeName:"",
+    myLandscapeId:"",
+    score:4.5,//景点的评分
+    picture:"",//景点的照片
     /*景点的照片*/
     dhzList: [
       '/images/dhz/1.jpg',
@@ -14,72 +17,157 @@ Page({
       '/images/dhz/3.jpg',
       '/images/dhz/4.jpg'
     ],
+    currentPage: 'introduction', //用于定位，默认页面为介绍
+    land_detail:[],//景点的详细信息
 
+
+    landscapeLocation: '广东省河源市连平县大湖寨',  // 景区地址
+    latitude: null,  // 纬度
+    longitude: null,  // 经度
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
+    console.log("options:",options);
     var that=this
+    var app=getApp()
    //进入该页面时传过来的参数指明了用户想要看哪个景点
     that.setData({
-      myLandscapeName:options
+      myLandscapeName:options.name,
+      myLandscapeId:options.id,
+      score:options.score,
+      picture:options.picture,
     }, function() {
-      // 在 setData 完成后执行
-      console.log("此时myLandscapeName的值为", that.data.myLandscapeName);
+     //获取该景点的信息
+     that.getInfo()
+      console.log("获取到的景点的图片的地址：",options.picture);
+    });
+
+  },
+
+  //跳转到去评论的页面
+  navigateToComment(){
+    wx.navigateTo({
+      url: `../fillComment/fillComment?name=${this.data.myLandscapeName}&id=${this.data.myLandscapeId}`,// 去填写评价的页面
     });
   },
 
+   //获取该景点的信息们
+   getInfo(){
+    var that=this
+    wx.request({
+      url: 'http://localhost:8080/landscape/getById',//后端获取单个景点的接口
+      method: 'GET',
+      data: {
+        landscape_id:that.data.myLandscapeId,
+      },
+      header:{
+        'content-type': 'application/x-www-form-urlencoded',
+      },
+      success(res) {
+        console.log('获取该景点信息的接口请求成功:', res.data.data);
+        that.setData({
+          land_detail:res.data.data
+        })
+        //这里是想获得该景点的图片们？？？？？？？？？？？
+    },
+      fail: function(error) {
+        // 处理登录失败的情况
+        console.log('获取单个景点信息失败:', error);
+        wx.showToast({
+            title: '网络错误，请重启小程序',
+            icon: 'none',
+            duration: 2000
+        });
+      }
+    })
+  },
+  
   getSwiperList(){
 
   },
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
+  
+   // 切换到介绍部分
+   goToIntro() {
+    this.setData({
+      currentPage: 'introduction',
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
+  // 切换到评价页面
+  goToComments() {
+    this.setData({
+      currentPage: 'comments',
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
+  // 获取景区的经纬度
+  getLocationByName: function() {
+    var that = this;
+    wx.request({
+      url: 'https://apis.map.qq.com/ws/geocoder/v1/', // 腾讯位置服务API的URL
+      data: {
+        address: "广东省河源市连平县大湖镇"+that.data.myLandscapeName, // 景区全名
+        key: '', // 这里要替换为腾讯地图API密钥？？？？？？？？？？？？？？？？？？？？？？？？
+      },
+      success: function(res) {
+        if (res.data.status === 0) {
+          // 获取经纬度
+          var location = res.data.result.location;
+          that.setData({
+            latitude: location.lat,
+            longitude: location.lng,
+          });
+          that.navigateToLocation(); // 获取到经纬度后导航
+        } else {
+          wx.showToast({
+            title: '无法获取位置',
+            icon: 'none',
+          });
+        }
+      },
+      fail: function() {
+        wx.showToast({
+          title: '网络错误',
+          icon: 'none',
+        });
+      },
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
+  // 导航到景区
+  navigateToLocation: function() {
+    var that = this;
+    console.log("——————————————————————导航到景区————————————————————————");
+    if (that.data.latitude && that.data.longitude) {
+      wx.openLocation({
+        latitude: that.data.latitude,   // 纬度
+        longitude: that.data.longitude, // 经度
+        scale: 18,                      // 地图缩放比例
+        name: that.data.myLandscapeName, // 景区名称
+        address: that.data.landscapeLocation // 景区地址
+      });
+    } else {
+      wx.showToast({
+        title: '位置未获取到',
+        icon: 'none',
+      });
+    }
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+  // 拨打电话
+  makePhoneCall: function() {
+    var that=this
+    wx.makePhoneCall({
+      phoneNumber: that.data.land_detail.telephone,  // 使用页面中定义的电话号码
+      success: function() {
+        console.log("电话拨打成功");
+      },
+      fail: function() {
+        console.log("电话拨打失败");
+      }
+    });
   }
-})
+});
