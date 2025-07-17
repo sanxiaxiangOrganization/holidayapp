7cf5d5c13ea450f8c7a3647b59678cffc131693c
Aqingxing110 <2946789832@qq.com>
2025-07-17 22:55:05 +0800

    实现用户订单展示

--- miniprogram-3/pages/mine_order/mine_order.js
+++ miniprogram-3/pages/mine_order/mine_order.js
@@ -1,1 +1,13 @@
-
+// pages/mine_order/mine_order.js
+Page({
+  data: {
+    orders: []
+  },
+  onLoad() {
+    // 从本地存储中获取订单数据
+    const orders = wx.getStorageSync('orders') || [];
+    this.setData({
+      orders
+    });
+  }
+});
