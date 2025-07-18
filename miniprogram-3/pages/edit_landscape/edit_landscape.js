08e39219db21e9e374031e1ce37d137e7b61ba63
Aqingxing110 <2946789832@qq.com>
2025-07-18 11:49:24 +0800

    删除images依赖

--- miniprogram-3/pages/edit_landscape/edit_landscape.js
+++ miniprogram-3/pages/edit_landscape/edit_landscape.js
@@ -50,8 +50,8 @@ Page({
     } catch (error) {
       console.error('加载图标失败:', error);
       this.setData({
-        deleteIcon: '/images/icons/delete.png',
-        cameraIcon: '/images/icons/camera.png',
+        deleteIcon: 'https://dhz-tourism-1329017069.cos.ap-guangzhou.myqcloud.com/icons/delete.png',
+        cameraIcon: 'https://dhz-tourism-1329017069.cos.ap-guangzhou.myqcloud.com/icons/camera.png'
       });
       throw error;
     }
